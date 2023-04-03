3ef1e97b55201cc4a6ae455960d1d6fb59ad83d7
Mariam-chiburdanidzee <123465456+Mariam-chiburdanidzee@users.noreply.github.com>
2023-04-03 23:28:41 +0400

    finish modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,30 +28,19 @@ const handleBurgerBar = () => {
 burger.addEventListener("click", handleBurgerBar);
 
 
-// Get the modal
 const modal = document.querySelector(".modal");
-
-// Get the button that opens the modal
 const signUpBtn = document.querySelector(".slide-button");
-
-// Get the <span> element that closes the modal
 const closeBtn = document.querySelector(".close");
-
-// When the user clicks on the button, open the modal
 signUpBtn.addEventListener("click", function() {
   modal.style.display = "block";
 });
 
-// When the user clicks on <span> (x), close the modal
 closeBtn.addEventListener("click", function() {
   modal.style.display = "none";
 });
 
-// When the user clicks anywhere outside of the modal, close it
 window.addEventListener("click", function(event) {
   if (event.target === modal) {
     modal.style.display = "block";
   }
 });
-
-//
